237d8ce3978efede64b0ce0acd87cac4ed8e23fc
Ryan Delaney <rrdelaney@outlook.com>
2017-10-08 20:02:41 -0400

    More Flow :D

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,3 +1,5 @@
+// @flow
+
 const { promisify } = require('util')
 const rimraf = promisify(require('rimraf'))
 
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,15 +1,17 @@
+// @flow
+
 const { promisify } = require('util')
 const fs = require('fs')
-const pLimit = require('p-limit')
 const { publish } = require('./lib/packages')
 
-const readdir = promisify(fs.readdir)
-const limit = pLimit(5)
+const readdir /*: (dirName: string) => Promise<string[]> */ = promisify(
+  fs.readdir
+)
 
 async function run() {
   const packages = await readdir('packages')
 
-  await Promise.all(packages.map(p => limit(() => publish(`packages/${p}`))))
+  await Promise.all(packages.map(p => publish(`packages/${p}`)))
 }
 
 run().catch(e => console.error(e.message))
